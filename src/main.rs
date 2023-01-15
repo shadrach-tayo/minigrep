4ee1436b50213f36609f3c9d23005f2cf6c9ff93
shadrach-tayo <shadrachtemitayo@gmail.com>
2023-01-15 05:17:18 +0100

    add tests, case insensitive version of search function, print to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::{env, fs};
+use std::{env, process};
+
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
-
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {}", query);
-    println!("In File {}", file_path);
-
-    let contents = fs::read_to_string(file_path).expect("Couldn't read the file");
-    println!("With text:\n{contents}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(e) =  minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
